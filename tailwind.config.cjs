6ffdec4f1dca80b7d08a52e18fbf89df84f60679
pexeixv <gavineditz@gmail.com>
2023-08-19 17:16:05 +0530

    create Home, About, Contact

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jakarta: [
+          "Plus Jakarta Sans Variable",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        dropShadow: {
+          "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "4xl": [
+            "0 35px 35px rgba(0, 0, 0, 0.25)",
+            "0 45px 65px rgba(0, 0, 0, 0.15)",
+          ],
+        },
+      },
+    },
+  },
+  plugins: [],
+};
