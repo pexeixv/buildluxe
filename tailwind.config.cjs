e2fc1d3330c487fef944f908bf50fc608f70f3f2
pexeixv <gavineditz@gmail.com>
2023-09-05 00:04:29 +0530

    create land, restoration pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,18 +4,24 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#e1b4bb",
+        },
+      },
       fontFamily: {
         jakarta: [
           "Plus Jakarta Sans Variable",
           ...defaultTheme.fontFamily.sans,
         ],
-        dropShadow: {
-          "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
-          "4xl": [
-            "0 35px 35px rgba(0, 0, 0, 0.25)",
-            "0 45px 65px rgba(0, 0, 0, 0.15)",
-          ],
-        },
+        catchye: ["Catchye", ...defaultTheme.fontFamily.sans],
+      },
+      dropShadow: {
+        "3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
+        "4xl": [
+          "0 35px 35px rgba(0, 0, 0, 0.25)",
+          "0 45px 65px rgba(0, 0, 0, 0.15)",
+        ],
       },
     },
   },
