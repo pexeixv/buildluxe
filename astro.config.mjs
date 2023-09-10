ec747d82e7d6c510eeaf063495da0671a607f0fd
pexeixv <gavineditz@gmail.com>
2023-09-10 10:50:21 +0530

    integrate form on Contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
   image: {
     service: sharpImageService(),
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
 });
